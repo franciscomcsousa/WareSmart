2815d3620b71d6d071bd65baa22ad89f27f3e392
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-02-27 16:25:32 +0000

    Added flask base

---
+++ backend/app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)@app.route('/')
+
+def home():
+    return render_template('home.html')@app.route('/about')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
