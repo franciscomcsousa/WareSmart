0c09d08c77ada83759e2ee3fde1065687614d8d1
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-03-01 18:44:22 +0000

    Remove useless example templates

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, make_response
 
-app = Flask(__name__)@app.route('/')
+app = Flask(__name__)
 
+@app.route('/')
 def home():
-    return render_template('home.html')@app.route('/about')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
