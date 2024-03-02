6a63162b74d88d0fe9a2520571d56f42048ca85f
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-03-02 18:55:04 +0000

    Added http requests to the application

---
+++ frontend/application/lib/httpRequests.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<Sensors> fetchSensors() async {
+  final response = await http
+      .get(Uri.parse('http://127.0.0.1:5000/sensors'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Sensors.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to read sensors');
+  }
+}
+
+class Sensors {
+  final int temperature;
+  final int humidity;
+  final int light;
+
+  const Sensors({
+    required this.temperature,
+    required this.humidity,
+    required this.light,
+  });
+
+  factory Sensors.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'temperature': int temperature,
+        'humidity': int humidity,
+        'light': int light,
+      } =>
+        Sensors(
+          temperature: temperature,
+          humidity: humidity,
+          light: light,
+        ),
+      _ => throw const FormatException('Failed to read sensors.'),
+    };
+  }
+}
